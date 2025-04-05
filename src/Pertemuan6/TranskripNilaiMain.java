54f86ba468c97a53215cd8d6416f8e6fced58339
rikirikandi <rikirikandi1242@gmail.com>
2025-04-05 20:13:54 +0700

    push

--- src/Pertemuan6/TranskripNilaiMain.java
+++ src/Pertemuan6/TranskripNilaiMain.java
@@ -7,17 +7,16 @@ public class TranskripNilaiMain {
 
         Dosen dosen1 = new Dosen("Dr.Ir Leony Lidya,MT", "0412106801");
         Dosen dosen2 = new Dosen("Fajar Darmawan, ST., M.Kom", "0402088504");
-        Dosen dosen3 = new Dosen("Sandika Galih S.T., M.T.", "0425098502");
-        Dosen dosen4 = new Dosen("Siroj Nur Ulum, S.T., M.T.", "2124129103");
+        Dosen dosen3 = new Dosen("Siroj Nur Ulum, S.T., M.T.", "2124129103");
+        Dosen dosen4 = new Dosen("Sandika Galih S.T., M.T.", "0425098502");
         Dosen dosen5 = new Dosen("Ade Sukendar, S.T., M.T.", "0429018502");
 
         // Create object matakuliah
         Matakuliah mk1 = new Matakuliah("001","Algoritma Pemrograman 1", "B", 3, dosen1);
         Matakuliah mk2 = new Matakuliah("002","Algoritma Pemrograman 2", "A", 3, dosen2);
-        Matakuliah mk3 = new Matakuliah("003","PBO", "A", 3, dosen4);
+        Matakuliah mk3 = new Matakuliah("003","Pemograman Berorientasi Objek", "A", 3, dosen3);
+        Matakuliah mk5 = new Matakuliah("005","Pemograman Web", "A", 3, dosen4);
         Matakuliah mk4 = new Matakuliah("004","Rekayasa Perangkat Lunak", "B", 3, dosen5);
-        Matakuliah mk5 = new Matakuliah("005","Pemograman Web", "A", 3, dosen3);
-
 
         // membuat object khs
         KartuHasilStudi khs = new KartuHasilStudi("2");
@@ -27,7 +26,6 @@ public class TranskripNilaiMain {
         khs.addMataKuliah(mk5);
         khs2.addMataKuliah(mk4);
         khs2.addMataKuliah(mk3);
-
 
         // Membuat object mahasiswa
         Mahasiswa mhs = new Mahasiswa("233040025", "Riki");
@@ -42,5 +40,4 @@ public class TranskripNilaiMain {
 
         transkrip.display();
     }
-
 }
